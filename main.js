2f6b938e50187ce9e2e1b595718defb9faa8fb97
Jeff DiPallo <jdipallo@gmail.com>
2016-06-02 18:16:47 -0600

    Completed Part I & II

--- main.js
+++ main.js
@@ -9,27 +9,24 @@ function mapController() {
 	// our data structure to hold all our data for each map marker
 	mc.mapMarkerList = [];
 
-	mc.addMapMarker = function(theEvent) {
-		var widthOfParent = theEvent.target.width;
-		console.log("Width of Thing You clicked on: ", widthOfParent);
+  mc.addMapMarker    = function(theEvent) {
+  var widthOfParent  = theEvent.target.width;
+  var heightOfParent = theEvent.target.height;
 
-		mc.mapMarkerList.push({
-			x: event.pageX,
-		 	y: event.pageY,
-		 	notes: ''
-		})
-	};
+	// we are calculating and setting our x & y points to be percentages 
+	// based on where the user clicks in relation to the width/height of 
+	// the container that is calling this controller
+	mc.mapMarkerList.push({
+		x: (event.pageX / widthOfParent)  * 100,
+	 	y: (event.pageY / heightOfParent) * 100,
+	 	notes: ''
+	})
+};
 
 	mc.removeMapMarker = function(mapMarkerIndex) {
 		mc.mapMarkerList.splice(mapMarkerIndex, 1);
 	};
-
-	// mc.calculateXpercent(x) {
-
-	// }
-	// mc.calculateXpercent(y) {
-		
-	// }
 }
+
 
 
